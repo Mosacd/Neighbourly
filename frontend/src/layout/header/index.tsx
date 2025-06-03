83626cbcfddf9a55c8d8eb578728f1feabb90cbd
Mosacd <levanmosiashvili5@gmail.com>
2025-06-03 12:55:17 +0400

    navigation setuo

--- frontend/src/layout/header/index.tsx
+++ frontend/src/layout/header/index.tsx
@@ -3,8 +3,15 @@ import globe from "@/assets/globe.svg"
 import { Button } from "@/components/ui/button";
 import ToggleSwitch from "@/components/ui/custom-elements/customToggle";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import GEO from "@/assets/lang/georgia 1.svg";
+import RUS from "@/assets/lang/russia 1.svg"
+import ENG from "@/assets/lang/usa 1.svg"
+import { useState } from "react";
 
 const Header = () => {
+
+    const [selectedLang, setSelectedLang] = useState("ENG");
+
   return (
   <div className="w-full h-[140px] py-[22px] px-[120px] shadow-bottom">
     <div className="flex justify-between">
@@ -18,15 +25,15 @@ const Header = () => {
           <Button variant={"secondary"}>
             Event Catalog
           </Button>
-        <Select defaultValue="ENG">
+        <Select defaultValue="ENG" onValueChange={setSelectedLang}>
   <SelectTrigger className="w-[116px] data-[size=default]:h-[53px] text-[24px] border-none noto-sans-regular">
     <img src={globe} alt="" />
-    <SelectValue />
+    <SelectValue>{selectedLang}</SelectValue>
   </SelectTrigger>
   <SelectContent className="min-w-[116px]">
-    <SelectItem className="text-[24px]" value="GEO">GEO</SelectItem>
-    <SelectItem className="text-[24px]" value="ENG">ENG</SelectItem>
-    <SelectItem className="text-[24px]" value="RUS">RUS</SelectItem>
+    <SelectItem className="text-[24px]" value="GEO"><img src={GEO} alt="" />GEO</SelectItem>
+    <SelectItem className="text-[24px]" value="ENG"><img src={ENG} alt="" />ENG</SelectItem>
+    <SelectItem className="text-[24px]" value="RUS"><img src={RUS} alt="" />RUS</SelectItem>
   </SelectContent>
 </Select>
 <ToggleSwitch/>
