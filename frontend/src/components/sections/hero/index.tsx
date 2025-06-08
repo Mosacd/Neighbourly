211be2a09daf6c20f07bb40f38cf1b94747b7b6d
Mosacd <levanmosiashvili5@gmail.com>
2025-06-08 23:52:32 +0400

    minor fix

--- frontend/src/components/sections/hero/index.tsx
+++ frontend/src/components/sections/hero/index.tsx
@@ -7,9 +7,9 @@ const Hero = () => {
         <div className="flex justify-between items-center w-full max-w-[1552px] mt-[120px] m-auto">
             <img src={hero} alt="heroImage" />
             <div className="w-full max-w-[590px]">
-                <h1 className="noto-sans-semibold text-[40px]">Become a Volunteer With <span className="piedra-regular text-main tracking-[2px] text-[64px]">Us</span></h1>
+                <h1 className="noto-sans-semibold text-[40px] mb-10">Become a Volunteer With <span className="piedra-regular text-main tracking-[2px] text-[64px]">Us</span></h1>
                 <p className="noto-sans-regular text-[24px]">Discover meaningful volunteer opportunities and join a community that's changing the world</p>   
-                <Button className="mt-15" variant={"default"}>Start Exploring</Button>     
+                <Button className="mt-10" variant={"default"}>Start Exploring</Button>     
             </div>
         </div>
     )
