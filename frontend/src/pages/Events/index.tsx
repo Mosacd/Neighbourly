3b66de974e9e4bf125defe2f220df8b14a71869f
Mosacd <levanmosiashvili5@gmail.com>
2025-06-08 17:35:46 +0400

    cards complete

--- frontend/src/pages/Events/index.tsx
+++ frontend/src/pages/Events/index.tsx
@@ -1,11 +1,23 @@
-
+import CardForEventList from "@/components/ui/custom-elements/cards/cardVariant3";
+import { eventData } from "@/dummyData";
 
 const Events = () => {
 
     return (
-        <>
         
-        </>
+        <div className="mt-[144px]">
+            <div>
+
+            </div>
+            <div className="">
+                <div className="flex flex-col items-center gap-[24px]">
+                {eventData.map((data) => {
+                    return  <CardForEventList data={data}/>
+                })}
+               </div>
+            </div>
+        </div>
+       
     )
 }
 
--- frontend/src/pages/singleEvent/index.tsx
+++ frontend/src/pages/singleEvent/index.tsx
@@ -17,7 +17,10 @@ const SingleEvent = () => {
         <div className="noto-sans-semibold text-[24px] max-w-[1680px] m-auto mt-[80px] mb-[120px]">{location.pathname.slice(1)} / Animal Shelter Helper Day</div>
         <div className="flex justify-between gap-[20px] w-full max-h-[801px] max-w-[1680px] m-auto mb-[200px]">
             <div className="flex flex-col gap-[40px] justify-between max-w-[830px] w-full">
-               <div>
+               <div className="overflow-y-auto pr-5"  style={{
+    scrollbarWidth: "thin",
+  }}>
+               <div className="mb-[73px]">
                 <h1 className="text-[48px] noto-sans-semibold">
             {eventData[convertedId-1]?.title}
                 </h1>
@@ -28,13 +31,9 @@ const SingleEvent = () => {
                     <p className="noto-sans-semibold text-[24px]">🌍Location - {eventData[convertedId-1]?.location}</p>
                     <p className="noto-sans-semibold text-[24px]">📅Schedule - {eventData[convertedId-1]?.schedule}</p>
                     </div>
-                <p  style={{
-    scrollbarWidth: "thin",            // Firefox
-    scrollbarColor: "#FFA412 white",    // Firefox (thumb color, track color)
-  
-  }} className="noto-sans-regular text-[24px] max-h-[385px] overflow-y-auto">
-                   {eventData[convertedId-1]?.description}
-                </p>
+                <p className="noto-sans-regular text-[24px] max-h-[385px]">
+                   {eventData[convertedId-1]?.description} </p>
+                </div>
                 </div>
                 <div className="flex gap-10">
                 <Button variant={"default"} className="max-w-[264px] w-full">Sign Up</Button>
