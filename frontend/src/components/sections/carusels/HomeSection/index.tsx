75e499ab36d63f6f8585aa44d035c78e2164fd1d
Mosacd <levanmosiashvili5@gmail.com>
2025-06-09 23:38:26 +0400

    home page carusel is responsive for phones as well, chrome and edge responsiveness issue fixed by setting 2xl breakpoint to 1537px

--- frontend/src/components/sections/carusels/HomeSection/index.tsx
+++ frontend/src/components/sections/carusels/HomeSection/index.tsx
@@ -3,12 +3,12 @@ import CaruselHome from "@/components/ui/custom-elements/carusels/customCarusel1
 
 const CaruselSection = () => {
 
-return(    <div className="h-[640px] 2xl:h-[720px] w-full bg-[#FFF5D7]">
+return(    <div className="h-[720px] md:h-[640px] 2xl:h-[720px] w-full bg-[#FFF5D7]">
                 <div className="m-auto flex flex-col items-center justify-center py-[26px]">
-                            <h1 className="noto-sans-semibold text-3xl 2xl:text-4xl mb-[48px]">
+                            <h1 className="noto-sans-semibold text-2xl md:text-3xl 2xl:text-4xl mb-[32px] md:mb-[48px]">
                             Upcoming Events
                             </h1>
-                                        <div className="w-full flex justify-center px-[70px]">
+                                        <div className="w-full flex justify-center md:px-[70px]">
 
                             <CaruselHome/>
                             </div>
