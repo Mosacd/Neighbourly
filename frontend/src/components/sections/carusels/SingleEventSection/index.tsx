c3e439fa1d84f9be1e26d3e5a5804bb32f0c867d
Mosacd <levanmosiashvili5@gmail.com>
2025-06-10 16:00:18 +0400

    text sizes fix

--- frontend/src/components/sections/carusels/SingleEventSection/index.tsx
+++ frontend/src/components/sections/carusels/SingleEventSection/index.tsx
@@ -5,7 +5,7 @@ const CaruselSection = () => {
 
 return(    
                 <div className="m-auto flex flex-col items-center justify-center py-[26px]">
-                            <h1 className="noto-sans-semibold text-2xl md:text-3xl 2xl:text-4xl mb-[32px] md:mb-[48px]]">
+                            <h1 className="noto-sans-semibold text-2xl md:text-3xl 2xl:text-4xl mb-[32px] md:mb-[48px]">
                             Similar Events
                             </h1>
                             
