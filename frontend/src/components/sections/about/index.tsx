5653c22e91670881b264c0a24b697b1160e29b80
Mosacd <levanmosiashvili5@gmail.com>
2025-06-14 02:40:54 +0400

    changed pictures for dark mode

--- frontend/src/components/sections/about/index.tsx
+++ frontend/src/components/sections/about/index.tsx
@@ -10,10 +10,10 @@ const About = () => {
                     <h1 className="mb-[24px] md:mb-[48px] noto-sans-semibold text-2xl sm:text-3xl 2xl:text-4xl ">Our Mission</h1>
                     <p className="noto-sans-regular text-md sm:text-lg 2xl:text-xl ">We believe in the power of people to create change. Our mission is to connect passionate volunteers with meaningful opportunities that make a real impact in communities. Together, we aim to build a kinder, stronger, and more supportive world—one act of service at a time.</p>
                 </div>
-            <img className="w-full max-w-[518px] 2xl:max-w-[658px]" src={pic1} alt="" />
+            <img className="w-full max-w-[518px] 2xl:max-w-[658px] dark:rounded-full" src={pic1} alt="" />
             </div>
             <div className="flex-col-reverse">
-            <img className="w-full max-w-[518px] 2xl:max-w-[658px]" src={pic2} alt="" />
+            <img className="w-full max-w-[518px] 2xl:max-w-[658px] dark:rounded-full" src={pic2} alt="" />
              <div className="max-w-[518px] 2xl:max-w-[688px]">
                     <h1 className="mb-[24px] md:mb-[48px] noto-sans-semibold text-2xl sm:text-3xl 2xl:text-4xl">Why Choose Us</h1>
                     <p className="noto-sans-regular text-md  sm:text-lg 2xl:text-xl">We make volunteering simple, impactful, and meaningful. Whether you're looking to give back, gain experience, or be part of something bigger, we connect you with causes that matter. With a supportive team, flexible opportunities, and a shared passion for change, we make it easy for you to make a difference.</p>
