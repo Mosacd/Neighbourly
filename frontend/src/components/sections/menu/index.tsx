a7c7ecd8986bf5a18fe3e951b38cb78f63ede531
Mosacd <levanmosiashvili5@gmail.com>
2025-06-16 21:17:42 +0400

    all the svg errors fixed, changed menu

--- frontend/src/components/sections/menu/index.tsx
+++ frontend/src/components/sections/menu/index.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import ToggleSwitch from "@/components/ui/custom-elements/customToggle";
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Link } from "react-router-dom";
 // import burgerMenuIcon from "@/assets/burger-menu-icon.svg"
 import LanguageSelect from "@/components/ui/custom-elements/customLangSelect";
@@ -11,15 +11,15 @@ const Menu = () => {
   return (
     <div className="flex lg:hidden">
       <Sheet>
-        <SheetTrigger className="">
+        <SheetTrigger className="group w-fit p-0 !px-1 bg-transparent dark:bg-transparent shadow-none hover:shadow-none border-none hover:cursor-pointer hover:bg-accent dark:hover:bg-accent duration-300 rounded-sm">
            <div className="flex lg:hidden items-center">
-          <Button variant={"outline"} className="group w-fit p-0 !px-1 bg-transparent dark:bg-transparent shadow-none hover:shadow-none border-none">
+          {/* <Button variant={"outline"} className="group w-fit p-0 !px-1 bg-transparent dark:bg-transparent shadow-none hover:shadow-none border-none"> */}
           {/* <img className="shrink-0 sm:size-11" src={burgerMenuIcon} alt="" /> */}
           <svg  className="shrink-0 sm:size-11 stroke-black dark:stroke-secondary-foreground" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M4 8H28M4 16H28M4 24H28" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M4 8H28M4 16H28M4 24H28" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 
-          </Button>
+          {/* </Button> */}
         </div>
         </SheetTrigger>
         <SheetContent side="left" className="w-full dark:text-white">
@@ -45,6 +45,7 @@ const Menu = () => {
               </div>
           
           </SheetHeader>
+        <SheetDescription></SheetDescription>
         </SheetContent>
       </Sheet>
     </div>
