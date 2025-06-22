fda26547e34fcb5a2d7cccd4a3233252908c55fd
Mosacd <levanmosiashvili5@gmail.com>
2025-06-22 10:49:53 +0400

    sign up and sign in forms complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,23 +6,25 @@ import SingleEvent from './pages/singleEvent'
 import Events from './pages/Events'
 import AuthLayout from './layouts/AuthLayout'
 import SignInForm from './pages/SignIn'
+import SignUpForm from './pages/SignUp'
 
 function App() {
   
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="Dashboard/Home" replace />} />
+      <Route path="/" element={<Navigate to="/Dashboard/Home" replace />} />
       <Route path="Dashboard" element={<DashboardLayout />}>
         {/* <Route index element={<Navigate to="/home" />} /> */}
         <Route path="Home" element={<Home />} />
         <Route path="Events" element={<Events />} />
         <Route path="Events/:id" element={<SingleEvent />} />
-        <Route path="*" element={<Navigate to="/home" replace />} />
       </Route>
       <Route path="Auth" element={<AuthLayout />}>
+        <Route index element={<Navigate to="/Auth/SignIn" replace />} />
         <Route path="SignIn" element={<SignInForm />} />
-        <Route path="SignUp" element={<Home />} />
+        <Route path="SignUp" element={<SignUpForm />} />
       </Route>
+      <Route path="*" element={<Navigate to="/Dashboard/home" replace />} />
     </Routes>
   )
 }
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -9,6 +9,7 @@ import { z } from "zod";
 
 
 const formSchema = z.object({
+  username: z.string().nonempty({ message: "Username is required" }),
   email: z
     .string()
     .email({ message: "Invalid email address" })
@@ -19,12 +20,13 @@ const formSchema = z.object({
     .nonempty({ message: "Password is required" }),
 });
 
-const SignInForm = () => {
+const SignUpForm = () => {
 
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      username: "",
       email: "",
       password: "",
     },
@@ -39,14 +41,32 @@ const SignInForm = () => {
   }
 
   return (
-    <div className="flex flex-col w-full  max-sm:p-0 max-sm:border-none max-w-[345px] items-center gap-[32px] 2xl:gap-[40px] py-[32px] sm:max-w-[448px] 2xl:max-w-[520px] rounded-[32px] border-[1px] border-[#636363]">
-      <h1 className="noto-sans-semibold text-[24px] 2xl:text-[32px]">Sign In</h1>
+    <div className="flex flex-col w-full max-sm:p-0 items-center gap-[32px] max-sm:border-none 2xl:gap-[40px] py-[32px] max-w-[345px] sm:max-w-[448px] 2xl:max-w-[520px] rounded-[32px] border-[1px] border-[#636363]">
+      <h1 className="noto-sans-semibold text-[24px] 2xl:text-[32px]">Sign Up</h1>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-[16px] 2xl:gap-[24px] max-w-[345px] sm:max-w-[368px] 2xl:max-w-[440px] w-full dark:text-white"
+          className="flex flex-col gap-[24px] max-w-[345px] sm:max-w-[368px] 2xl:max-w-[440px] w-full dark:text-white"
         >
           <div className="flex flex-col gap-[12px] 2xl:gap-[16px]">
+             <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="noto-sans-semibold text-[14px] 2xl:text-[16px]">Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-neutral-200 h-[42px] 2xl:h-[48px] rounded-[8px] noto-sans-semibold"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="email"
@@ -79,13 +99,27 @@ const SignInForm = () => {
                 </FormItem>
               )}
             />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="noto-sans-semibold text-[14px] 2xl:text-[16px]">Confirm Passowrd</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-neutral-200 h-[42px] 2xl:h-[48px] rounded-[8px] noto-sans-semibold"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
     </div>
 <div className="flex flex-col justify-center items-center gap-[24px] 2xl:gap-[32px]">
-              {/* <Link > */}
-                  <span className="text-[14px] 2xl:text-[16px] self-start  noto-sans-semibold text-main-green dark:text-white dark:hover:text-neutral-300 font-semibold hover:text-neutral-800 hover:underline hover:cursor-pointer">
-                    Forgot Password?
-                  </span>
-              {/* </Link> */}
+            
               {/* {isError && (
                 <p className="text-red-600 text-md">
                   {error?.message || "Invalid login credentials"}
@@ -102,13 +136,11 @@ const SignInForm = () => {
               
             </div>
           <FormDescription className="flex justify-center gap-2">
-            <span className="text-[#726f6f] text-[14px] 2xl:text-[16px] noto-sans-semibold">Don’t have an account?</span>
-            <span  className="text-[14px] 2xl:text-[16px] noto-sans-semibold self-start text-black  noto-sans-regular text-main-green dark:text-white dark:hover:text-neutral-300 font-semibold hover:text-neutral-800 hover:underline hover:cursor-pointer">SIGN UP</span>
-          </FormDescription>
+                  </FormDescription>
         </form>
       </Form>
     </div>
   );
 };
 
-export default SignInForm;
+export default SignUpForm;
